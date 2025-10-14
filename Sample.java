0c07267986f11de66872917e5bcc66c675bf3075
Andy Wu <andywu@eduroam-int-dhcp-97-10-56.ucl.ac.uk>
2025-10-14 14:36:40 +0100

    feat: Initial population of code similarity database

--- Sample.java
+++ Sample.java
@@ -1,9 +1,9 @@
 class Sample {
     Sample() {}
-    
+
     int add(int a, int b) { return a + b; }
 
-    int minus(int a, int b) { return a - b; }
+    int adawdawdawd(int a, int b) { return a / b; }
 
     static { System.out.println("init"); }  // initializer (no name)
 }
