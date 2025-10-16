8a9728b2aff6e60e51e729263f80a5a10de194a2
Andy Wu <andywu@Andys-MacBook-Air.local>
2025-10-16 10:24:28 +0100

    new added test file

---
+++ hello.py
@@ -0,0 +1,20 @@
+def hello_world():
+    print("Hello, world!")
+    
+
+def add(a, b):
+    return a + b
+
+
+def hello(name):
+    for i in range(5):
+        print(f"Hello, {name}!")
+        
+def hi(name):
+    for i in range(5):
+        print(f"Hello, {name}!")
+        
+        
+def heyyyy(name):
+    for i in range(5):
+        print(f"Hello, {name}!")
