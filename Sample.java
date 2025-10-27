3497b42395c73eaab687a5428b790a7d7bf8d52d
Andy Wu <andywu@Andys-MacBook-Air.local>
2025-10-27 22:57:49 +0000

    testing

--- Sample.java
+++ Sample.java
@@ -3,15 +3,11 @@ class Sample {
 
     int add(int a, int b) { return a + b; }
 
-    int hello(int a, int b) { return a + b; }
-
     int minus(int a, int b) { return a - b; }
-
-    int abcdefc(int a, int b) { return a - b; }
-
-    int heyhey(int a, int b) { return a - b; }
-
+    
     int greeting456(int a, int b) { return a - b; }
+
+    int greeting123(int a, int b) { return a - b; }
 
     static { System.out.println("init"); } 
 }
