4a8b26e1282fac3ea935a5b6c7328afec0370139
Andy Wu <andywu@Andys-Air.Home>
2025-11-10 12:42:31 +0000

    Testing Code Changes

--- src/test_files/sample_python.py
+++ src/test_files/sample_python.py
@@ -6,11 +6,11 @@ def add(a, b):
     return a + b
 
 
-def heyalice(name):
+def heyace(name):
     for i in range(5):
         print(f"Hello, {name}!")
         
-def heybob(name):
+def heytrix(name):
     for i in range(5):
         print(f"Hello, {name}!")
         
