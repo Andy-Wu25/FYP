ef44868ef3a3c5d10499b5261a7c9389bab46f02
Andy Wu <andywu@Andys-MacBook-Air.local>
2025-11-11 10:29:22 +0000

    Testing Deletion from DB

--- src/test_files/sample_python.py
+++ src/test_files/sample_python.py
@@ -5,10 +5,6 @@ def hello_world():
 def add(a, b):
     return a + b
 
-
-def helhjfgjhfghjlohello(name):
-    for i in range(5):
-        print(f"Hello, {name}!")
         
 def awdawdwad(name):
     for i in range(5):
